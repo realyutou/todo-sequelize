d7ed4d4f641ad2163e1e6c694526289967bc5562
realyutou <ssps9430@gmail.com>
2023-07-30 17:19:55 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
+const session = require('express-session')
 const bcrypt = require('bcryptjs')
 
 const db = require('./models')
@@ -15,6 +16,13 @@ const PORT = 3000
 // Set template engine
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
+// Set express-session
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 
 // Set body-parser
 app.use(express.urlencoded({ extended: true }))
